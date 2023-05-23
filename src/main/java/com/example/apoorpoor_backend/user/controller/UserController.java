005a66b38ac2c3c8774a4ab3238c83ab6d7c4638
eivomin <eivomiin@gmail.com>
2023-05-23 15:25:29 +0900

    Feat: 소셜로그인 기능 구현

---
+++ src/main/java/com/example/apoorpoor_backend/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.apoorpoor_backend.user.controller;
+
+import com.example.apoorpoor_backend.user.dto.UserSignUpDto;
+import com.example.apoorpoor_backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
+        userService.signUp(userSignUpDto);
+        return "회원가입 성공";
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
+    }
+}

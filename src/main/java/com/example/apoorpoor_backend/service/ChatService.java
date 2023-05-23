265b1073ffcd6110f5adc41d0b3c2e74050dd29e
inggaeng <kce0_0@naver.com>
2023-05-23 21:43:48 +0900

    Fix : 채팅

---
+++ src/main/java/com/example/apoorpoor_backend/service/ChatService.java
@@ -0,0 +1,24 @@
+package com.example.apoorpoor_backend.service;
+
+import com.example.apoorpoor_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class ChatService {
+    private static final String S3_BUCKET_PREFIX = "S3";
+
+    private final UserRepository userRepository;
+    private final ChatRepository chatRepository;
+    private final ChatRoomRepository chatRoomRepository;
+
+    public ChatDto enterChatR
+
+
+}

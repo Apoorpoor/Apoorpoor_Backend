19526236cf12298ccd44a6849d8d3f4e0b219c1e
tiktokyi <tiktokyi@gmail.com>
2023-05-24 16:28:22 +0900

    Feat : 가계부 Entity 추가

---
+++ src/main/java/com/example/apoorpoor_backend/entity/FinancialLedger.java
@@ -0,0 +1,41 @@
+package com.example.apoorpoor_backend.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "TB_FINANCIAL_LEDGER")
+@Getter
+public class FinancialLedger extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String incomeType;
+
+    @Column
+    private String expenditureType;
+
+    @Column
+    private  Long income;
+
+    @Column
+    private Long expenditure;
+
+    @Column
+    private Long totalAmount;
+
+    @Column
+    private LocalDateTime dateTime;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}

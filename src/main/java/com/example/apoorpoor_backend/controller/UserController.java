90f527a6f5fe17391bc3ce13d41a226e1490f1e0
eivomin <eivomiin@gmail.com>
2023-05-31 20:20:51 +0900

    Fix: application.properties 수정

---
+++ src/main/java/com/example/apoorpoor_backend/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.apoorpoor_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+}

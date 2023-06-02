813e6067d42ac6da263d3faa25b65052c458204b
DESKTOP-HQGLTLL\leesehoon <peacockblue70@naver.com>
2023-06-02 18:05:51 +0900

    Feat: s3 이미지 업로드 기능 추가

--- src/main/java/com/example/apoorpoor_backend/controller/ChatController.java
+++ src/main/java/com/example/apoorpoor_backend/controller/ChatController.java
@@ -1,6 +1,10 @@
 package com.example.apoorpoor_backend.controller;
 
 import com.example.apoorpoor_backend.model.ChatMessage;
+import com.example.apoorpoor_backend.service.S3Uploader;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -18,6 +22,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
+    private final S3Uploader s3Uploader;
 
     @MessageMapping("/chat/enter")
     @SendTo("/sub/chat/room")
@@ -30,5 +35,14 @@ public class ChatController {
     @SendTo("/sub/chat/room")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
+    }
+
+
+    @ResponseBody
+    @PostMapping(value = "/chat/image",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadImage(@RequestParam(value = "image", required = false)MultipartFile image, @AuthenticationPrincipal UserDetails userDetails)throws IOException{
+        String image_url = s3Uploader.uploadImage(image);
+        return image_url;
+        //return chatService.uploadImage(image);
     }
 }
---
+++ src/main/java/com/example/apoorpoor_backend/model/ExceptionEnum.java
@@ -0,0 +1,5 @@
+package com.example.apoorpoor_backend.model;
+
+public enum ExceptionEnum {
+
+}

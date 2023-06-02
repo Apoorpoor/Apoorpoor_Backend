505efd5fa5ca86d0153b5fe57d6910fbbf81efd8
tiktokyi <tiktokyi@gmail.com>
2023-06-02 23:16:22 +0900

    Feat : 레벨업 포인트 지점 선정

--- src/main/java/com/example/apoorpoor_backend/model/enumType/LevelType.java
+++ src/main/java/com/example/apoorpoor_backend/model/enumType/LevelType.java
@@ -1,9 +1,16 @@
 package com.example.apoorpoor_backend.model.enumType;
 
 public enum LevelType {
-    LV1(1L, 500L),
-    LV2(2L, 1000L),
-    LV3(3L, 1600L);
+    LV1(1L, 100L),
+    LV2(2L, 300L),
+    LV3(3L, 600L),
+    LV4(4L, 1000L),
+    LV5(5L, 1500L),
+    LV6(6L, 2100L),
+    LV7(7L, 2800L),
+    LV8(8L, 3600L),
+    LV9(9L, 4500L),
+    LV10(10L, 5500L);
 
     private final Long level;
     private final Long nextExp;

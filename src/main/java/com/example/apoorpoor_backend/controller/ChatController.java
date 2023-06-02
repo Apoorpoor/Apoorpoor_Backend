f73f572de64b42cfab3ee239a8abcb918cd36188
DESKTOP-HQGLTLL\leesehoon <peacockblue70@naver.com>
2023-06-02 18:05:11 +0900

    Feat: s3 이미지업로드기능 추가

--- src/main/java/com/example/apoorpoor_backend/controller/ChatController.java
+++ src/main/java/com/example/apoorpoor_backend/controller/ChatController.java
@@ -5,20 +5,29 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.register")
-    @SendTo("/topic/public")
+    @MessageMapping("/chat/enter")
+    @SendTo("/sub/chat/room")
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 
-    @MessageMapping("/chat.send")
-    @SendTo("/topic/public")
+    @MessageMapping("/chat/send")
+    @SendTo("/sub/chat/room")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
     }

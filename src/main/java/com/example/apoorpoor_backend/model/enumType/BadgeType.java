804034934a40c87ead773235cedaaf520e6aded3
tiktokyi <tiktokyi@gmail.com>
2023-06-03 13:55:29 +0900

    Mod : 뱃지 enum  변경

--- src/main/java/com/example/apoorpoor_backend/model/enumType/BadgeType.java
+++ src/main/java/com/example/apoorpoor_backend/model/enumType/BadgeType.java
@@ -4,19 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum BadgeType {
-    BADGE1(1L,"집주인님 이번만요"),
-    BADGE2(2L, "축의금은 얼마?"),
-    BADGE3(3L, "열차 들어옵니다."),
-    BADGE4(4L, "여보세요?"),
-    BADGE5(5L, "이달의 보험왕"),
-    BADGE6(6L, "공부의 신"),
-    BADGE7(7L, "나는 문화인"),
-    BADGE8(8L, "아프면 손드세요"),
-    BADGE9(9L, "3대 500"),
-    BADGE10(10L, "햄 버억"),
-    BADGE11(11L, "Flex 했지 뭐얌"),
-    BADGE12(12L, "#여유 #휴식"),
-    BADGE13(13L, "티끌 모아 태산");
+    HOME(1L,"집주인님 이번만요"),
+    TRIBUTE(2L, "축의금은 얼마?"),
+    TRANSPORTATION(3L, "열차 들어옵니다."),
+    COMMUNICATION(4L, "여보세요?"),
+    INSURANCE(5L, "이달의 보험왕"),
+    EDUCATION(6L, "공부의 신"),
+    CULTURE(7L, "나는 문화인"),
+    MEDICAL(8L, "아프면 손드세요"),
+    HEALTH(9L, "3대 500"),
+    FOOD(10L, "햄 버억"),
+    SHOPPING(11L, "Flex 했지 뭐얌"),
+    LEISURE(12L, "#여유 #휴식"),
+    DEPOSIT(13L, "티끌 모아 태산");
 
     private final Long badgeNum;
 

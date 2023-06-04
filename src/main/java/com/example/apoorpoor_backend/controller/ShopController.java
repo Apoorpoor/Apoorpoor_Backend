e4b20b69ab5245b35aacba7db8ec3c7716f0b407
tiktokyi <tiktokyi@gmail.com>
2023-06-04 17:47:48 +0900

    Fix : 상점 물품 조회 기능 정상 작동 되도록 보수

--- src/main/java/com/example/apoorpoor_backend/controller/ShopController.java
+++ src/main/java/com/example/apoorpoor_backend/controller/ShopController.java
@@ -1,16 +1,18 @@
 package com.example.apoorpoor_backend.controller;
 
 import com.example.apoorpoor_backend.dto.BeggarExpUpResponseDto;
+import com.example.apoorpoor_backend.dto.shop.ItemListResponseDto;
 import com.example.apoorpoor_backend.dto.shop.PayRequestDto;
 import com.example.apoorpoor_backend.security.UserDetailsImpl;
 import com.example.apoorpoor_backend.service.ShopService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "ShopController", description = "상점 controller")
 @RestController
@@ -19,13 +21,13 @@ public class ShopController {
 
     private final ShopService shopService;
 
-//    @Operation(summary = "상점 물품 목록 조회 API" , description = "상점 물품 조회")
-//    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "상품 리스트 조회 완료" )})
-//    @GetMapping("/shop")
-//    public ResponseEntity<ItemListResponseDto> getItemList(
-//            @RequestParam("itemType") String itemType) {
-//        return shopService.getItemList(itemType);
-//    }
+    @Operation(summary = "상점 물품 목록 조회 API" , description = "상점 물품 조회")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "상품 리스트 조회 완료" )})
+    @GetMapping("/shop")
+    public ResponseEntity<ItemListResponseDto> getItemList(
+            @RequestParam("itemType") String itemType) {
+        return shopService.getItemList(itemType);
+    }
 
     @PatchMapping("/pay")
     public ResponseEntity<BeggarExpUpResponseDto> buyPointUpdate(@RequestBody

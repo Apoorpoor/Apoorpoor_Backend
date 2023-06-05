bad0f505874a75c352503553eea368e53a1bcf3a
DESKTOP-HQGLTLL\leesehoon <peacockblue70@naver.com>
2023-06-05 23:31:03 +0900

    Fix : yml 파일 수정

---
+++ scripts/stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/apoorapoor"
+JAR="$ROOT_PATH/application-plain.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+
+NOW=$(date +%c)
+
+
+if [ -z "$SERVICE_PID" ]; then
+echo " [$NOW] 서비스 NouFound" >> $STOP_LOG
+else
+echo " [$NOW] [$SERVICE_PID] 서비스 종료 " >> $STOP_LOG
+kill "$SERVICE_PID"
+# kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi

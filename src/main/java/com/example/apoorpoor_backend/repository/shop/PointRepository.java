cd33e9feb0b2161a4b8c3e85fd0612eddcfab7e8
inggaeng <kce0_0@naver.com>
2023-06-06 23:28:10 +0900

    Feat : 포인트 목록 조회 기능 추가

--- src/main/java/com/example/apoorpoor_backend/repository/shop/PointRepository.java
+++ src/main/java/com/example/apoorpoor_backend/repository/shop/PointRepository.java
@@ -3,5 +3,5 @@ package com.example.apoorpoor_backend.repository.shop;
 import com.example.apoorpoor_backend.model.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointRepository extends JpaRepository<Point, Long> {
+public interface PointRepository extends JpaRepository<Point, Long>, PointRepositoryCustom {
 }

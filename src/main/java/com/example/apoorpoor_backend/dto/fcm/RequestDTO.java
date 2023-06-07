4ed6089b70ab8f7ca5dc6af52b6b8fa418c8f6ce
eivomin <eivomiin@gmail.com>
2023-06-07 16:14:04 +0900

    Feat: fcm 알림 기능 추가

---
+++ src/main/java/com/example/apoorpoor_backend/dto/fcm/RequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.apoorpoor_backend.dto.fcm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestDTO {
+    private String targetToken;
+    private String title;
+    private String body;
+}

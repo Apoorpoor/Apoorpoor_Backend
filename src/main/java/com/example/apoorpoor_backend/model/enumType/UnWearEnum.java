d2d2b6fa68e6a28fafd3cb6ab02eba3ccd98fa41
tiktokyi <tiktokyi@gmail.com>
2023-06-08 02:58:33 +0900

    Mod : 아이템, unWearEnum 변경

--- src/main/java/com/example/apoorpoor_backend/model/enumType/UnWearEnum.java
+++ src/main/java/com/example/apoorpoor_backend/model/enumType/UnWearEnum.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum UnWearEnum {
-    UN_WEAR_TOP("top"),
-    UN_WEAR_BOTTOM("bottom"),
-    UN_WEAR_SHOES("shoes"),
-    UN_WEAR_ACC("acc"),
-    UN_WEAR_CUSTOM("custom");
+    top("top"),
+    bottom("bottom"),
+    shoes("shoes"),
+    acc("acc"),
+    custom("custom");
 
     private final String unWearPart;
 

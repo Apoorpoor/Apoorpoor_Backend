33b47664d5ef8719b88c70ff494a8d383702d209
tiktokyi <tiktokyi@gmail.com>
2023-06-12 22:44:11 +0900

    Mod : FCMInit 주석

--- src/main/java/com/example/apoorpoor_backend/config/FCMInit.java
+++ src/main/java/com/example/apoorpoor_backend/config/FCMInit.java
@@ -1,35 +1,35 @@
-package com.example.apoorpoor_backend.config;
-
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-
-@Slf4j
-@Service
-public class FCMInit{
-    private static final String path = "firebase/apoorpoor-55170-firebase-adminsdk-mir91-a019045af1.json";
-
-    @PostConstruct
-    public void init(){
-        try{
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream())).build();
-
-
-            if(FirebaseApp.getApps().isEmpty()){
-                FirebaseApp.initializeApp(options);
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-}
+//package com.example.apoorpoor_backend.config;
+//
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import jakarta.annotation.PostConstruct;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.stereotype.Service;
+//
+//import java.io.IOException;
+//
+//@Slf4j
+//@Service
+//public class FCMInit{
+//    private static final String path = "firebase/apoorpoor-55170-firebase-adminsdk-mir91-a019045af1.json";
+//
+//    @PostConstruct
+//    public void init(){
+//        try{
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream())).build();
+//
+//
+//            if(FirebaseApp.getApps().isEmpty()){
+//                FirebaseApp.initializeApp(options);
+//            }
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}

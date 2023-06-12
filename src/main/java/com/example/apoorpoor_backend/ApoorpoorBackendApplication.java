d43e1243079071075657a4bf748b94e2b21149ad
inggaeng <kce0_0@naver.com>
2023-06-12 17:23:06 +0900

    Feat : 채팅 좋아요 기능

--- src/main/java/com/example/apoorpoor_backend/ApoorpoorBackendApplication.java
+++ src/main/java/com/example/apoorpoor_backend/ApoorpoorBackendApplication.java
@@ -2,6 +2,7 @@ package com.example.apoorpoor_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,7 +14,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @EnableJpaAuditing
-@EnableScheduling
+@EnableCaching
+@EnableScheduling //Caching
 @SpringBootApplication
 public class ApoorpoorBackendApplication {
 
--- src/main/java/com/example/apoorpoor_backend/service/ChatService.java
+++ src/main/java/com/example/apoorpoor_backend/service/ChatService.java
@@ -1,19 +1,26 @@
 package com.example.apoorpoor_backend.service;
 
+
 import com.example.apoorpoor_backend.dto.chat.ChatDto;
 import com.example.apoorpoor_backend.model.Beggar;
 import com.example.apoorpoor_backend.model.Chat;
 import com.example.apoorpoor_backend.model.enumType.MessageType;
 import com.example.apoorpoor_backend.repository.beggar.BeggarRepository;
 import com.example.apoorpoor_backend.repository.chat.ChatRepository;
+import com.google.common.cache.CacheBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -22,7 +29,10 @@ import java.util.Date;
 public class ChatService {
 
     private final BeggarRepository beggarRepository;
-    //private final ChatRepository chatRepository;
+    private final ChatRepository chatRepository;
+    private final RedisTemplate<String, ChatDto> chatRedisTemplate;
+    private static final String CHAT_LIKES_CACHE_KEY = "chat_likes";
+
 
     public ChatDto enterChatRoom(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("beggar_id", chatDto.getBeggar_id());
@@ -50,12 +60,30 @@ public class ChatService {
     public void sendChatRoom(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
         Beggar beggar = beggarCheck(chatDto.getBeggar_id());
         MessageType type = MessageType.TALK;
-//        Date date = new Date();
-//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        String dateformat = format.format(date);
-//        chatDto.setDate(dateformat);
-//        Chat chat = new Chat(chatDto,beggar, type);
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateformat = format.format(date);
+        chatDto.setDate(dateformat);
+        Chat chat = new Chat(chatDto,beggar, type);
     }
+
+    @CachePut(value = "chatLikes", key = "#chatId")
+   public void addLikeToChatMessage(Long chatId) {
+        Chat chat = chatRepository.findById(chatId).orElseThrow(
+                () -> new IllegalArgumentException("채팅 메시지를 찾을 수 없습니다.")
+        );
+        chat.setLikes(); // 좋아요 수 증가
+        chatRepository.save(chat);
+        // Redis 캐시에 저장
+        String cacheKey = generateCacheKey(chatId);
+        ValueOperations<String, ChatDto> valueOperations = chatRedisTemplate.opsForValue();
+        valueOperations.set(cacheKey, new ChatDto(chat), 30, TimeUnit.DAYS); // TTL 30일 설정
+    }
+
+    private String generateCacheKey(Long chatId) {
+        return CHAT_LIKES_CACHE_KEY + ":" + chatId;
+    }
+
 
     public Beggar beggarCheck(Long beggar_id) {
         return beggarRepository.findById(beggar_id).orElseThrow(

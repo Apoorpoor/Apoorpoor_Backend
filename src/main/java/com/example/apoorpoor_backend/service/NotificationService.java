b90cdce8ac467827b63b7b32d0dd90a1b86fb569
tiktokyi <tiktokyi@gmail.com>
2023-06-12 21:56:54 +0900

    Mod : sse service 수정

--- src/main/java/com/example/apoorpoor_backend/service/NotificationService.java
+++ src/main/java/com/example/apoorpoor_backend/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.example.apoorpoor_backend.service;
 
 import com.example.apoorpoor_backend.model.Beggar;
+import com.example.apoorpoor_backend.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -14,17 +15,18 @@ import static com.example.apoorpoor_backend.controller.SseController.sseEmitters
 @RequiredArgsConstructor
 public class NotificationService {
 
-    public void notifyGetBadgeEvent(Beggar beggar, String expenditureTitle) {
-        Long beggarId =beggar.getId();
+    public void notifyGetBadgeEvent(User user, Beggar beggar, String expenditureTitle) {
+        String userName = user.getUsername();
+        //Long beggarId =beggar.getId();
         String nickName = beggar.getNickname();
 
-        if(sseEmitters.containsKey(beggarId)) {
-            SseEmitter sseEmitter = sseEmitters.get(beggarId);
+        if(sseEmitters.containsKey(userName)) {
+            SseEmitter sseEmitter = sseEmitters.get(userName);
             try {
                 sseEmitter.send(SseEmitter.event().name("getBadge").data("{\"nickName\":\"" + nickName + "\", " + "\"msg :" + expenditureTitle + "뱃지를 획득 하였습니다!" + "\","
                 + LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM월 dd일 a HH시 mm분")) + "\" }"));
             } catch (Exception e) {
-                sseEmitters.remove(beggarId);
+                sseEmitters.remove(userName);
             }
         }
     }

dab4f6fddb8ddc6d9bae75c21a70cadaa536ad92
inggaeng <kce0_0@naver.com>
2023-06-17 15:17:40 +0900

    Feat : 채팅 참여자수, 목록

--- src/main/java/com/example/apoorpoor_backend/service/ChatService.java
+++ src/main/java/com/example/apoorpoor_backend/service/ChatService.java
@@ -1,16 +1,21 @@
 package com.example.apoorpoor_backend.service;
 
 import com.example.apoorpoor_backend.dto.chat.ChatDto;
+import com.example.apoorpoor_backend.dto.chat.ChatListDto;
 import com.example.apoorpoor_backend.model.Beggar;
 import com.example.apoorpoor_backend.model.Chat;
 import com.example.apoorpoor_backend.model.enumType.MessageType;
 import com.example.apoorpoor_backend.repository.beggar.BeggarRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -20,12 +25,19 @@ import java.util.Set;
 public class ChatService{
 
     private final BeggarRepository beggarRepository;
+    private final List<ChatListDto> chatParticipantsList = new ArrayList<>();
 
-    public ChatDto enterChatRoom(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
+    public ChatDto enterChatRoom(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor, ChatListDto chatListDto) {
         headerAccessor.getSessionAttributes().put("beggar_id", chatDto.getBeggar_id());
         headerAccessor.getSessionAttributes().put("nickName", chatDto.getSender());
         headerAccessor.getSessionAttributes().put("userId", chatDto.getUserId());
         chatDto.setMessage(chatDto.getSender() + "님이 입장하셨습니다.");
+
+        Long beggarId = chatDto.getBeggar_id();
+        chatListDto.getChatList().add(beggarId);
+
+        chatParticipantsList.add(chatListDto);
+
         return chatDto;
     }
 
@@ -41,6 +53,15 @@ public class ChatService{
                 .message(nickName + "님이 퇴장하셨습니다.")
                 .userId(userId)
                 .build();
+
+
+        chatParticipantsList.stream()
+                .filter(chatListDto -> chatListDto.getChatList().contains(beggar_id))
+                .findFirst()
+                .ifPresent(chatListDto -> {
+                    chatListDto.getChatList().remove(beggar_id);
+                });
+
         return chatDto;
     }
 
@@ -53,6 +74,10 @@ public class ChatService{
                 .beggar(beggar)
                 .type(type)
                 .build();
+    }
+
+    public List<ChatListDto> getChatParticipants() {
+        return new ArrayList<>(chatParticipantsList);
     }
 
     public Beggar beggarCheck(Long beggar_id) {

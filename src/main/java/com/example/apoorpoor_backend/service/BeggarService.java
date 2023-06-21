45f9ad8aeadf64f5524d2a408147adc21cf4dde4
eivomin <eivomiin@gmail.com>
2023-06-21 22:16:22 +0900

    Fix: merge

--- src/main/java/com/example/apoorpoor_backend/service/BeggarService.java
+++ src/main/java/com/example/apoorpoor_backend/service/BeggarService.java
@@ -67,7 +67,7 @@ public class BeggarService {
 
     public ResponseEntity<BeggarSearchResponseDto> myBeggar(String username) {
         User user = userCheck(username);
-        Beggar beggar = beggarCheckExp(username);
+        Beggar beggar = beggarCheck(username);
 
         Long beggarId = beggar.getId();
         Long userId = user.getId();
@@ -367,14 +367,6 @@ public class BeggarService {
                 () -> new IllegalArgumentException("푸어를 찾을 수 없습니다.")
         );
     }
-
-    private Beggar beggarCheckExp(String username) {
-
-        Optional<Beggar> findBeggar = beggarRepository.findByUsername(username);
-        if(findBeggar.isPresent()) return findBeggar.get();
-        else return null;
-    }
-
 
     public User userIdCheck(Long userId) {
         return userRepository.findById(userId).orElseThrow(

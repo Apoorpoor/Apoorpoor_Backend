6ae5590ef5674312a340c48f20877dc01bf56c5e
tiktokyi <tiktokyi@gmail.com>
2023-06-24 00:01:12 +0900

    Test : BeggarServiceTest 작성

--- src/test/java/com/example/apoorpoor_backend/service/BeggarServiceTest.java
+++ src/test/java/com/example/apoorpoor_backend/service/BeggarServiceTest.java
@@ -176,8 +176,8 @@ class BeggarServiceTest {
     }
 
     @Test
-    @DisplayName("아이템 착용")
-    public void customBeggar() {
+    @DisplayName("일반 아이템만 착용")
+    public void customBeggarTest() {
         //given
         String nickname = "TestBeggar";
 
@@ -193,6 +193,7 @@ class BeggarServiceTest {
 
         String itemType = itemListEnum.getItemType();
 
+        //when
         switch (itemType) {
             case "top" -> beggar.updateCustomTops(itemListEnum);
             case "bottom" -> beggar.updateCustomBottoms(itemListEnum);
@@ -202,8 +203,156 @@ class BeggarServiceTest {
             default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
         }
 
+        //then
+        assertEquals("top", itemType);
         assertEquals(itemListEnum, beggar.getTop());
         assertNull(beggar.getBottom());
+        assertNull(beggar.getShoes());
+        assertNull(beggar.getAcc());
+        assertNull(beggar.getCustom());
+
+    }
+
+    @Test
+    @DisplayName("커스텀 아이템 착용")
+    public void putOnCustomBeggarTest() {
+        //given
+        String nickname = "TestBeggar";
+
+        Beggar beggar = Beggar.builder()
+                .nickname(nickname)
+                .level(10L)
+                .exp(4500L)
+                .point(0L)
+                .build();
+
+        ItemListEnum itemListEnum = ItemListEnum.custom_lv10_01;
+        UnWearEnum unWearEnum = null;
+
+        String itemType = itemListEnum.getItemType();
+
+        //when
+        switch (itemType) {
+            case "top" -> beggar.updateCustomTops(itemListEnum);
+            case "bottom" -> beggar.updateCustomBottoms(itemListEnum);
+            case "shoes" -> beggar.updateCustomShoes(itemListEnum);
+            case "acc" -> beggar.updateCustomAccessories(itemListEnum);
+            case "custom" -> beggar.updateCustoms(itemListEnum);
+            default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
+        }
+
+        //then
+        assertEquals("custom", itemType);
+        assertNull(beggar.getTop());
+        assertNull(beggar.getBottom());
+        assertNull(beggar.getShoes());
+        assertNull(beggar.getAcc());
+        assertEquals(itemListEnum, beggar.getCustom());
+
+    }
+
+    @Test
+    @DisplayName("일반 아이템 착용후 커스텀 아이템 착용 테스트")
+    public void put_On_Custom_And_Top_customBeggarTest() {
+        //given
+        String nickname = "TestBeggar";
+
+        Beggar beggar = Beggar.builder()
+                .nickname(nickname)
+                .level(10L)
+                .exp(4500L)
+                .point(0L)
+                .build();
+
+        ItemListEnum itemListEnum = ItemListEnum.top_lv2_01;
+        ItemListEnum customItemListEnum = ItemListEnum.custom_lv10_01;
+        UnWearEnum unWearEnum = null;
+
+        String itemType = itemListEnum.getItemType();
+
+        //when_1
+        switch (itemType) {
+            case "top" -> beggar.updateCustomTops(itemListEnum);
+            case "bottom" -> beggar.updateCustomBottoms(itemListEnum);
+            case "shoes" -> beggar.updateCustomShoes(itemListEnum);
+            case "acc" -> beggar.updateCustomAccessories(itemListEnum);
+            case "custom" -> beggar.updateCustoms(itemListEnum);
+            default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
+        }
+
+        //중간 검증
+        assertEquals(itemListEnum, beggar.getTop());
+
+        //when_2
+        itemType = customItemListEnum.getItemType();
+
+        switch (itemType) {
+            case "top" -> beggar.updateCustomTops(itemListEnum);
+            case "bottom" -> beggar.updateCustomBottoms(itemListEnum);
+            case "shoes" -> beggar.updateCustomShoes(itemListEnum);
+            case "acc" -> beggar.updateCustomAccessories(itemListEnum);
+            case "custom" -> beggar.updateCustoms(itemListEnum);
+            default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
+        }
+
+        //then
+        assertNull(beggar.getTop());
+        assertNull(beggar.getBottom());
+        assertNull(beggar.getShoes());
+        assertNull(beggar.getAcc());
+        assertEquals(itemListEnum, beggar.getCustom());
+
+    }
+
+    @Test
+    @DisplayName("unWearEnum이 null이 아닐때 아이템 해당 부위 착용해제 테스트")
+    public void putOffCustomBeggarTest() {
+        //given
+        String nickname = "TestBeggar";
+
+        Beggar beggar = Beggar.builder()
+                .nickname(nickname)
+                .level(10L)
+                .exp(4500L)
+                .point(0L)
+                .top(ItemListEnum.top_lv2_01)
+                .build();
+
+        ItemListEnum itemListEnum = ItemListEnum.top_lv2_01;
+        UnWearEnum unWearEnum = UnWearEnum.top;
+
+        String itemType = itemListEnum.getItemType();
+
+        //when
+        if(unWearEnum != null) {
+            String unWear = unWearEnum.getUnWearPart();
+
+            switch (unWear) {
+                case "top" -> beggar.updateCustomTops(null);
+                case "bottom" -> beggar.updateCustomBottoms(null);
+                case "shoes" -> beggar.updateCustomShoes(null);
+                case "acc" -> beggar.updateCustomAccessories(null);
+                case "custom" -> beggar.updateCustoms(null);
+                default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
+            }
+        }else {
+                switch (itemType) {
+                    case "top" -> beggar.updateCustomTops(itemListEnum);
+                    case "bottom" -> beggar.updateCustomBottoms(itemListEnum);
+                    case "shoes" -> beggar.updateCustomShoes(itemListEnum);
+                    case "acc" -> beggar.updateCustomAccessories(itemListEnum);
+                    case "custom" -> beggar.updateCustoms(itemListEnum);
+                    default -> throw new IllegalArgumentException("옳지 못한 행동입니다.");
+                }
+            }
+
+
+        //then
+        assertNull(beggar.getTop());
+        assertNull(beggar.getBottom());
+        assertNull(beggar.getShoes());
+        assertNull(beggar.getAcc());
+        assertNull(beggar.getCustom());
 
     }
 
